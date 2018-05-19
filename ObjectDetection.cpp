78f0f8e644e1a111c782e1d7a0c669ae723ca333
SoullessStone <micheldc@windowslive.com>
2018-05-19 03:36:46 -0700

    trying to recognize numbers

--- ObjectDetection.cpp
+++ ObjectDetection.cpp
@@ -5,6 +5,11 @@
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/xfeatures2d.hpp"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -35,6 +40,7 @@ const char* keys =
 using namespace cv;
 using namespace dnn;
 using namespace std;
+using namespace cv::xfeatures2d;
 
 void postprocess(Mat& frame, const std::vector<Mat>& out, Net& net);
 
@@ -53,6 +59,9 @@ void showField();
 void test(Mat& image);
 
 void changeField(int x, int y, int newValue);
+
+int countSiftMatches(Mat& player, Mat& number);
+int getPossibilityForPlayerAndNumber(Mat& player, int number);
 
 float confThreshold;
 std::vector<std::string> classes;
@@ -210,8 +219,17 @@ void postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net)
 						else {
 							cout << "Black Player" << endl;
 						}
+
+						// Find number
+						cv::Mat greyPlayer;
+						cv::cvtColor(player, greyPlayer, cv::COLOR_BGR2GRAY);
+						std::array<int, 7> possibleNumbers = {1,3,4,5,6,8,9};
+						for(int& i: possibleNumbers) { 
+							cout << "------ Possibility for " << i << ": " << getPossibilityForPlayerAndNumber(greyPlayer, i) << endl;
+							//waitKey();
+						}
+
 						imshow(kWinName2, player);
-						//test(player);
 						waitKey(0);
 					}
 
@@ -233,6 +251,75 @@ void postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net)
 	}
 	else
 		CV_Error(Error::StsNotImplemented, "Unknown output layer type: " + outLayerType);
+}
+
+int getPossibilityForPlayerAndNumber(Mat& player, int number) {
+	Mat n = imread( "numbers/"+std::to_string(number)+".jpg", IMREAD_GRAYSCALE );
+	Mat nSmall;
+	cv::resize(n,nSmall,Size(45,60), 0, 0, cv::INTER_AREA);
+	int c1 = countSiftMatches(player, nSmall);
+	cv::resize(n,nSmall,Size(60,80), 0, 0, cv::INTER_AREA);
+	int c2 = countSiftMatches(player, nSmall);
+	cv::resize(n,nSmall,Size(90,120), 0, 0, cv::INTER_AREA);
+	int c3 = countSiftMatches(player, nSmall);
+	cv::resize(n,nSmall,Size(112,150), 0, 0, cv::INTER_AREA);
+	int c4 = countSiftMatches(player, nSmall);
+	//cout << "count autoscaled: " << std::to_string(c) << endl;
+	return c1+c2+c3+c4;
+}
+
+int countSiftMatches(Mat& player, Mat& number) {
+	
+	if( !player.data || !number.data )
+	{
+		std::cout<< " --(!) Error reading images " << std::endl;
+		return -1; 
+	}
+	//-- Step 1: Detect the keypoints using SIFT Detector, compute the descriptors
+	Ptr<SIFT> detector = SIFT::create();
+	std::vector<KeyPoint> keypoints_1, keypoints_2;
+	Mat descriptors_1, descriptors_2;
+	detector->detectAndCompute( player, Mat(), keypoints_1, descriptors_1 );
+	// TODO only calculate Keypoints&Descriptors once
+	detector->detectAndCompute( number, Mat(), keypoints_2, descriptors_2 );
+	//-- Step 2: Matching descriptor vectors using FLANN matcher
+	BFMatcher matcher;
+	std::vector< DMatch > matches;
+	matcher.match( descriptors_1, descriptors_2, matches );
+	double max_dist = 0; double min_dist = 100;
+	//-- Quick calculation of max and min distances between keypoints
+	for( int i = 0; i < descriptors_1.rows; i++ )
+	{ double dist = matches[i].distance;
+	if( dist < min_dist ) min_dist = dist;
+	if( dist > max_dist ) max_dist = dist;
+	}
+	//printf("-- Max dist : %f \n", max_dist );
+	//printf("-- Min dist : %f \n", min_dist );
+	//-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+	//-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+	//-- small)
+	//-- PS.- radiusMatch can also be used here.
+	std::vector< DMatch > good_matches;
+	for( int i = 0; i < descriptors_1.rows; i++ )
+	{ 
+		//printf("%f, min dist*2 = %f \n", matches[i].distance,2*min_dist);
+		if( matches[i].distance <= max(2*min_dist, 0.02) )
+		{ 
+			good_matches.push_back( matches[i]); 
+		}
+	}
+	//-- Draw only "good" matches
+	Mat img_matches;
+	drawMatches( player, keypoints_1, number, keypoints_2,
+		 good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+		 vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+	//-- Show detected matches
+	imshow( "Good Matches", img_matches );
+	return (int)good_matches.size();
+	//for( int i = 0; i < (int)good_matches.size(); i++ )
+	//{ 
+		//printf( "-- Good Match [%d] Keypoint 1: %d-- Keypoint 2: %d\n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); 
+	//}
 }
 
 void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
@@ -342,7 +429,6 @@ void showField() {
 void changeField(int x, int y, int newValue) {
 	field.at(y).at(x) = newValue;
 }
-
 
 
 
